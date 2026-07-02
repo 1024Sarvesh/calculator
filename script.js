4953aae82e77c124aa540d85ab87c2c4678f481d
Sarvesh Kumar <abcode1024@gmail.com>
2026-07-02 18:08:58 +0530

    operator inbuiding its working

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const display = document.getElementById("display")
 
-function append(value){
+function append(value) {
     display.value += value
 
 }
@@ -11,4 +11,17 @@ function clearDisplay() {
 
 function deleteLast() {
     display.value = display.value.slice(0, -1);
+}
+
+
+
+
+function calculate() {
+ 
+        let output = display.value;
+        let fun = Function("return " + output);
+        let result = fun();
+        display.value = result;
+
+    
 }
